45ba83b54c2c466a6681e1e674ba7666b80124df
Brandon Matulonis <brandoncmatulonis@gmail.com>
2021-10-29 15:24:54 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 // Define React Compenent
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby tutorial.</p>
+        <StaticImage
+          alt="FSU Logo"
+          src="../images/FSU.png"
+        />
       </Layout>
   )
 }
